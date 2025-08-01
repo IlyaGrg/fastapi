4de6e43c92830d1941c5e14b3456d943443061bf
Ilya <ilyagrigorevt@gmail.com>
2025-08-01 13:33:53 +0400

    Получение книги по айди

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , HTTPException
 import uvicorn
 
 app = FastAPI()
 
-@app.get("/" , summary = "Главная ручка" , tags = ["Основные ручки"])
+books = [
+    {
+        "id" : 1,
+        "title" : "Титька",
+        "autor": "Loru"
+    },
+    {
+        "id" : 2,
+        "title" : "Попка",
+        "autor" : "Gilza"
+    }
+]
+@app.get("/books")
+def read_books():
+    return books    
 
-def main():
-    return "Hello World"
+
+@app.get("/books/{id}")
+def get_book(book_id : int):
+    for book in books:
+        if book["id"] == book_id:
+            return book
+    raise HTTPException(status_code=404)
+    
 
 if __name__ == "__main__":
     uvicorn.run("main:app" , reload =True)
